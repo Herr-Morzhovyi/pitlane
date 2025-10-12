0699e6794d110805ee61b66df5f75eb98048a516
IvanB <ivan@MacBook-Pro-Ivan.local>
2025-10-12 09:45:32 +0100

    header and navigation

---
+++ src/app/header/header.ts
@@ -0,0 +1,17 @@
+import { Component, signal, EventEmitter, Output } from '@angular/core';
+import { PrimaryBtn } from '../common/primary-btn/primary-btn';
+
+@Component({
+  selector: 'app-header',
+  imports: [PrimaryBtn],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+  @Output() onClick = new EventEmitter<string>();
+  buttonText = signal('Contact us!');
+
+  scroll(el: string) {
+    this.onClick.emit(el);
+  }
+}
