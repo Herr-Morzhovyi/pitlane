694bf3c74b683c9f333ca1e7c43c1d5cd6f5f27e
IvanB <ivan@MacBook-Pro-Ivan.local>
2025-10-12 11:22:21 +0100

    header fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -31,6 +31,10 @@ export class App {
   }
 
   scroll(el: string) {
+    if (el === 'logo') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      return;
+    }
     document.querySelector(el)?.scrollIntoView({ behavior: 'smooth' });
   }
 }
