4ed359a8211c16e7e2fa2090b780ab006a571df0
Ivan B <ivanberezin@Ivans-MacBook-Air.local>
2025-11-16 19:57:06 +0000

    responsive fix, add sidenav on mobile

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,40 +1,55 @@
-import {Component, output, signal} from '@angular/core';
-import {PrimaryBtn} from '../common/primary-btn/primary-btn';
+import { Component, output, signal, inject, computed } from '@angular/core';
+import { PrimaryBtn } from '../common/primary-btn/primary-btn';
+import { DrawerModule } from 'primeng/drawer';
+import { BreakpointService } from '../services/breakpoint.service';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
-  imports: [PrimaryBtn],
+  imports: [PrimaryBtn, DrawerModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  #breakPoint = inject(BreakpointService);
+
   onClick = output<string>();
   buttonText = signal('Contact us!');
+  isDesktop = computed(() => {
+    return this.#breakPoint.isDesktop()?.matches;
+  });
+
+  visible: boolean = false;
 
   links = signal([
     {
       label: 'Home',
-      href: '#hero'
+      href: '#hero',
     },
     {
       label: 'Gallery',
-      href: '#gallery'
+      href: '#gallery',
     },
     {
       label: 'Pricing',
-      href: '#pricing'
+      href: '#pricing',
     },
     {
       label: 'Contact',
-      href: '#contact'
-    }
+      href: '#contact',
+    },
   ]);
 
   scroll(el: string) {
+    this.toggleNavBar();
     this.onClick.emit(el);
   }
 
   scrollTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  toggleNavBar() {
+    this.visible = !this.visible;
   }
 }
